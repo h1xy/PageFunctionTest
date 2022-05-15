0f909a5d937d3a57c038f0493d844d30212b5f73
h1xy <48129611+h1xy@users.noreply.github.com>
2022-05-16 00:03:58 +0800

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -10,6 +10,6 @@ export async function onRequest(context) {
     } = context;
 
     let url = new URL(request.url);
-    url.hostname = "h3xy.herokuapp.com";//该成自己的域名
+    url.hostname = "kr2.netfly.top";//该成自己的域名
     return fetch(new Request(url, request))
 }
